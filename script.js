890d3d0d49cb15b3a221c706121a5366bfd3adbe
RemiBoudin <boyer.remi81@gmail.com>
2015-06-22 15:13:29 +0200

    Javascript ajout fonction "sleep"

--- script.js
+++ script.js
@@ -141,3 +141,13 @@ if ( typeof ws !== 'undefined') {
 function log(txt) {
 	document.getElementById('log').innerHTML += txt + "<br>\n";
 }
+
+
+function sleep(milliseconds) {
+    var start = new Date().getTime();
+    for (var i = 0; i < 1e7; i++) {
+        if ((new Date().getTime() - start) > milliseconds) {
+            break;
+        }
+    }
+}
